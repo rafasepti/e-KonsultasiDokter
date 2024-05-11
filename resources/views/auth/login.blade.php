7f55e9ce40a71edd6d08449b2af45a0ffaa5f107
rafasepti <rafasepti@gmail.com>
2024-05-11 09:07:59 +0700

    update: login register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,12 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            <a class="mr-2 underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __('Belum punya akun? Daftar') }}
+            </a>
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Lupa password?') }}
                 </a>
             @endif
 
